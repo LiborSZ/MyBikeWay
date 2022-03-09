21ec1ee1d57644993244b9baaf3b9803623e255a
LiborSZ <reins20@seznam.cz>
2022-03-09 08:41:40 +0100

    Project language switched to Enhglish

---
+++ MyBikeWay/Location.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBikeWay
+{
+    internal class Location
+    {
+        /// <summary>
+        /// Location name (city / village etc.)
+        /// </summary>
+        public string Name { get; private set; }
+        /// <summary>
+        /// Coordinate X in decimal degrees (format xx.xxxxx)
+        /// </summary>
+        public double CoordinateX { get; private set; }
+        /// <summary>
+        /// Coordinate Y in decimal degrees (format yy.yyyyyy)
+        /// </summary>
+        public double CoordinateY { get; private set; }
+        /// <summary>
+        /// Distance in Km from default point
+        /// </summary>
+        public int StartingPointDistance { get; private set; }
+        /// <summary>
+        /// Constructor with properties inicialization
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="CoordinateX"></param>
+        /// <param name="CoordinateY"></param>
+        /// <param name="StartingPointDistance"></param>
+        public Location(string name, double coordinateX, double coordinateY, int distance)
+        {
+            Name = name;
+            CoordinateX = coordinateX;
+            CoordinateY = coordinateY;
+            StartingPointDistance = distance;
+        }
+        /// <summary>
+        /// Returns name and distance from default point as string
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Format($"{Name} je vzdálený od výchozí lokace o {StartingPointDistance}");
+        }
+    }
+}
