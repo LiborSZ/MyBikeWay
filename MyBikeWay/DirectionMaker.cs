e0fcb8f03ad0a346ed74845285d84c10e476586d
LiborSZ <reins20@seznam.cz>
2022-03-13 09:25:12 +0100

    Created new class DirectionMaker with instance of database LocationsDB, instance of current date and time and ctor for DB inicialization

---
+++ MyBikeWay/DirectionMaker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBikeWay
+{
+    internal class DirectionMaker
+    {
+        /// <summary>
+        /// Instance of LocationsDB databse
+        /// </summary>
+        private LocationsDB database;
+        /// <summary>
+        /// Instance of current date and time.
+        /// </summary>
+        private DateTime currentDate = DateTime.Now;
+        /// <summary>
+        /// Constructor inicializing the database LocationsDB
+        /// </summary>
+        public DirectionMaker()
+        {
+            database = new LocationsDB();
+        }
+
+
+
+
+    } 
+}
