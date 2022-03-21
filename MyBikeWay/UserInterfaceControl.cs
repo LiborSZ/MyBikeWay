df6f5cebed0335345b2ea464052994da70f1686c
LiborSZ <reins20@seznam.cz>
2022-03-21 18:10:59 +0100

    added and inicializated LocationDbUserControl / DirectionMaker via constuctor

--- MyBikeWay/UserInterfaceControl.cs
+++ MyBikeWay/UserInterfaceControl.cs
@@ -8,5 +8,24 @@ namespace MyBikeWay
 {
     internal class UserInterfaceControl
     {
+        /// <summary>
+        /// Instance of LocationDbUserControl
+        /// </summary>
+        LocationDbUserControl locationUser;
+        /// <summary>
+        /// Instance of DirectionMaker
+        /// </summary>
+        DirectionMaker direction;
+        /// <summary>
+        /// Constructor inicializating LocationDbUserControl and DirectionMaker classes
+        /// </summary>
+        public UserInterfaceControl()
+        {
+            locationUser = new LocationDbUserControl();
+            direction = new DirectionMaker();
+        }
+
+        
+
     }
 }
