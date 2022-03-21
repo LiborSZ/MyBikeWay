796dad3d9c5824080dc46ef8dabe5ba573f34ba9
LiborSZ <reins20@seznam.cz>
2022-03-21 12:06:39 +0100

    Removed methods which belongs under new class LocationDbUserControl

--- MyBikeWay/DirectionMaker.cs
+++ MyBikeWay/DirectionMaker.cs
@@ -9,9 +9,16 @@ namespace MyBikeWay
     internal class DirectionMaker
     {
         /// <summary>
-        /// Instance of LocationsDB databse
+        /// Instance of LocationsDB class
         /// </summary>
         private LocationsDB database;
+        /// <summary>
+        /// Instance of LocationDbUserControl class
+        /// </summary>
+        private LocationDbUserControl dbControl;
+        /// <summary>
+        /// Instance of ValidationMethods Class
+        /// </summary>
         private ValidationMethods validator;
         /// <summary>
         /// Instance of current date and time.
@@ -22,48 +29,14 @@ namespace MyBikeWay
         /// </summary>
         private LinkedList<Location> directions;
         /// <summary>
-        /// Constructor inicializing the database LocationsDB
+        /// Constructor inicializing DB, user, validation classes and linked list
         /// </summary>
         public DirectionMaker()
         {
             database = new LocationsDB();
             directions = new LinkedList<Location>();
             validator = new ValidationMethods();
-        }
-        /// <summary>
-        /// Method for getting user input for adding new location to the database with coordinates
-        /// </summary>
-        public Location AddLocationWithCoordinates()
-        {
-            Console.WriteLine("Insert location name");
-            string text ="";
-            validator.EmptyStringValid(text);
-            double distance =0;
-            Console.WriteLine("Insert distance from previous point (inser 0 if default point)");
-            validator.DoubleValid(distance);
-            Console.WriteLine("Insert coordinate X");
-            double x =0;
-            validator.DoubleValid(x);
-            double y=0;
-            Console.WriteLine("Insert coordinate Y");
-            validator.DoubleValid(y);
-
-            database.AddLoaction(text, x, y, distance);
-            return database.returnLast();
-        }
-        /// <summary>
-        /// Method for getting user input for adding new location to the database without coordinates
-        /// </summary>
-        public Location AddLocationWithoutCoordinates()
-        {
-            Console.WriteLine("Insert location name");
-            string text="";
-            validator.EmptyStringValid(text);
-            double distance =0;
-            Console.WriteLine("Insert coordinate distance from previous point");
-            validator.DoubleValid(distance);
-            database.AddLoaction(text, distance);
-            return database.returnLast();
+            dbControl = new LocationDbUserControl();
         }
         /// <summary>
         /// Add location into direction linked list or DB
@@ -73,11 +46,11 @@ namespace MyBikeWay
         {  
                 if (withCoordinates)
                 {
-                    directions.AddLast(AddLocationWithCoordinates());
+                    directions.AddLast(dbControl.AddLocationWithCoordinates());
                 }
                 else
                 {
-                    directions.AddLast(AddLocationWithoutCoordinates());
+                    directions.AddLast(dbControl.AddLocationWithoutCoordinates());
                 }
             
         }
@@ -111,14 +84,7 @@ namespace MyBikeWay
         {
             directions.Remove(database.FindLocation(name));
         }
-        /// <summary>
-        /// Method for removing location from DB based on name
-        /// </summary>
-        /// <param name="name"></param>
-        public void RemoveLocationDB(string name)
-        {
-            database.DeleteLocation(name);
-        }
+        
         /// <summary>
         /// Add location into direction based on existing location
         /// </summary>
@@ -130,49 +96,7 @@ namespace MyBikeWay
             directions.AddLast(database.FindLocation(name));
         }
 
-        /// <summary>
-        /// Method for location atribute update
-        /// </summary>
-        /// <param name="name"></param>
-        public void UpdateLocation()
-        {
-            string name ="";
-            name = validator.StringValid(name);
-            string attribute ="";
-            string newName ="";
-            double newCoordinate = 0;
-            Console.WriteLine("Name | Distance | Location X | Location Y | Exit");
-            Console.WriteLine("Enter attribute to update or exit: ");
 
-            Console.WriteLine(attribute);
-            attribute = validator.StringValid(attribute);
-            switch (attribute) 
-            {
-                case "name":
-                    validator.EmptyStringValid(newName);
-                    database.UpdateLocationName(name, newName);
-                    break;
-                case "distance":
-                    validator.DoubleValid(newCoordinate);
-                    database.UpdateLocationDistance(name, newCoordinate);
-                    break;
-                case "location x":
-                    validator.DoubleValid(newCoordinate);
-                    database.UpdateLocationDistance(name, newCoordinate);
-                    break;
-                case "location y":
-                    validator.DoubleValid(newCoordinate);
-                    database.UpdateLocationDistance(name, newCoordinate);
-                    break;
-                case "exit":
-                    break;
-                    default:
-                    Console.WriteLine("Enter correct attribute to update or exit");
-                    break;
-
-            }
-
-        }
         
     } 
     
