90c5d9699ce26b9d92d7c6d03648281237d0a84d
LiborSZ <reins20@seznam.cz>
2022-03-21 06:37:06 +0100

    Inser input data validation class with double and string validation methods created

---
+++ MyBikeWay/ValidationMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBikeWay
+{
+    internal class ValidationMethods
+    {
+        public double DoubleValid(double number)
+        {
+            while (!double.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Please enter correct number: ");
+            }
+            return number;
+        }
+
+        public string StringValid(string txt)
+        {
+            
+            while (string.IsNullOrEmpty(txt = Console.ReadLine().ToLower().Trim()))
+            {
+                Console.WriteLine("Please enter correct text: ");
+            }
+            return txt;
+        }
+       
+    }
+}
