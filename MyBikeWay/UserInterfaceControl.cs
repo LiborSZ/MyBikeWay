92242217af3963b15a7ed884603a505e7e60302f
LiborSZ <reins20@seznam.cz>
2022-03-21 18:42:44 +0100

    Console opening interface method created

--- MyBikeWay/UserInterfaceControl.cs
+++ MyBikeWay/UserInterfaceControl.cs
@@ -13,6 +13,10 @@ namespace MyBikeWay
         /// </summary>
         LocationDbUserControl locationUser;
         /// <summary>
+        /// Instance of current date and time.
+        /// </summary>
+        private DateTime currentDate = DateTime.Now;
+        /// <summary>
         /// Instance of DirectionMaker
         /// </summary>
         DirectionMaker direction;
@@ -25,7 +29,18 @@ namespace MyBikeWay
             direction = new DirectionMaker();
         }
 
-        
+        public void OpeningInterface()
+        {
+            Console.WriteLine("\n--------------------Welcome in the MyBikeWay application--------------------");
+            Console.WriteLine("This application was created to save locations and make directions from them");
+            Console.WriteLine("----------------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Locations Database");
+            Console.WriteLine("------------------");
+            Console.WriteLine("Please select operation: \n1 - Create new Location with coordinates \n2 - Create new location without coordinates" +
+                "\n3 - Remove location \n4 - Update location \n5 - Switch to direction making \n6 - Exit");
+
+        }
 
     }
 }
