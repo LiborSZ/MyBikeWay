cefe855451649c427e37b46746c288d214fa0c59
LiborSZ <reins20@seznam.cz>
2022-03-21 11:49:06 +0100

    Added method to valid correct string and added comments for all methods

--- MyBikeWay/ValidationMethods.cs
+++ MyBikeWay/ValidationMethods.cs
@@ -8,24 +8,48 @@ namespace MyBikeWay
 {
     internal class ValidationMethods
     {
+
+        string text;
+        /// <summary>
+        /// Method to valid correct double user input
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
         public double DoubleValid(double number)
         {
             while (!double.TryParse(Console.ReadLine(), out number))
             {
-                Console.WriteLine("Please enter correct number: ");
+                Console.WriteLine("Please enter correct number or decimal number: ");
             }
             return number;
         }
-
-        public string StringValid(string txt)
+        /// <summary>
+        /// Method to valid if input string is not null or empty
+        /// </summary>
+        /// <param name="txt"></param>
+        /// <returns></returns>
+        public string EmptyStringValid(string txt)
         {
             
+            while (string.IsNullOrEmpty(txt = Console.ReadLine().ToLower().Trim()))
+            {
+                Console.WriteLine("Input cannot be empty, please enter again: ");
+            }
+            return txt;
+        }
+        /// <summary>
+        /// Method to valid correct string user input
+        /// </summary>
+        /// <param name="txt"></param>
+        /// <returns></returns>
+        public string StringValid(string txt)
+        {
+
             while (string.IsNullOrEmpty(txt = Console.ReadLine().ToLower().Trim()))
             {
                 Console.WriteLine("Please enter correct text: ");
             }
             return txt;
         }
-       
     }
 }
