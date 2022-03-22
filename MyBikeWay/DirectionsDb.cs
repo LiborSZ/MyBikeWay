d3758dcc66bcaef3469d2c2682fb210ea09aee23
LiborSZ <reins20@seznam.cz>
2022-03-22 14:17:18 +0100

    Directions Db class updated with add find and delete methods

--- MyBikeWay/DirectionsDb.cs
+++ MyBikeWay/DirectionsDb.cs
@@ -8,5 +8,67 @@ namespace MyBikeWay
 {
     internal class DirectionsDb
     {
+        /// <summary>
+        /// Direaction maker class 
+        /// </summary>
+        private List<DirectionMaker> directions;
+        /// <summary>
+        /// Direction maker class initialized
+        /// </summary>
+        public DirectionsDb()
+        {
+            directions = new List<DirectionMaker>();
+        }
+        /// <summary>
+        /// Method for adding direction into list
+        /// </summary>
+        /// <param name="name"></param>
+        public void AddDirection(string name)
+        {
+            directions.Add(new DirectionMaker(name));
+        }
+
+        /// <summary>
+        /// LINQ query to find and return Direction by name
+        /// </summary>
+        /// <param name="name">Location name</param>
+        /// <returns>Location if found</returns>
+        public DirectionMaker FindDirection(string name)
+        {
+
+            var DirectionQuery = from direction in directions
+                                where direction.Name == name
+                                select direction;
+            foreach (DirectionMaker direction in DirectionQuery)
+            {
+                return direction;
+            }
+            Console.WriteLine("Direction was not found");
+            return null;
+
+        }
+
+        /// <summary>
+        /// Removing method for directions DB
+        /// </summary>
+        /// <param name="name">Location name</param>
+        public void DeleteDirection(string name)
+        {
+            List<DirectionMaker> found = new List<DirectionMaker>();
+            found.Add(FindDirection(name));
+            foreach (DirectionMaker direction in found)
+            {
+                if (direction.Name == name)
+                {
+                    directions.Remove(direction);
+                    Console.WriteLine("Location {0} removed", name);
+                }
+                else
+                {
+                    Console.Write("Location was not found");
+                }
+            }
+
+        }
     }
 }
